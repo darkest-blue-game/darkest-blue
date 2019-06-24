a19791ffb0f458834edfd45c7b491af7d0e0dad8
Padmapriya Ganapathi <gpadmapriya@hotmail.com>
2019-06-24 14:39:47 -0700

    Added player constructor for testing

--- js/model.js
+++ js/model.js
@@ -1,1 +1,14 @@
 'use strict';
+
+//Player constructor
+var Player = function (name) {
+  this.name = name;
+  this.remainingHealthPoints = 0;
+  this.numberOfCardsPlayed = 0;
+  this.numberOfTurnsPlayed = 0;
+  this.nextTurn = false;
+
+  //array of players
+  Player.allPlayers.push(this);
+};
+
