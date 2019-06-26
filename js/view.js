f17831768559072b80aa88c0a940bcffdc85f30f
Brandon Hurrington <brandonhurrington@yahoo.com>
2019-06-26 12:47:53 -0700

    CSS domination.

--- js/view.js
+++ js/view.js
@@ -1,39 +1,42 @@
 'use strict';
 
+
+
+
 // const canvas = document.getElementById('gameBoard');
 // const ctx = canvas.getContext('2d');
 
-var drawBackground = function () {
-  var context = document.getElementById('background-layer').getContext('2d');
+// var drawBackground = function () {
+//   var context = document.getElementById('background-layer').getContext('2d');
 
-  var gradient = context.createRadialGradient(110, 90, 30, 100, 100, 70);
-  gradient.addColorStop(0, "white");
-  gradient.addColorStop(0.9, "#201d57");
-  gradient.addColorStop(1, "black");
+//   var gradient = context.createRadialGradient(110, 90, 30, 100, 100, 70);
+//   gradient.addColorStop(0, "white");
+//   gradient.addColorStop(0.9, "#201d57");
+//   gradient.addColorStop(1, "black");
   
-  context.fillStyle = gradient;
-  context.fillRect(10, 10, 400, 400);
-};
+//   context.fillStyle = gradient;
+//   context.fillRect(10, 10, 400, 400);
+// };
 
-drawBackground();
+// drawBackground();
 
-var drawGameBoard = function () {
-  var context = document.getElementById('game-board-layer').getContext('2d');
+// var drawGameBoard = function () {
+//   var context = document.getElementById('game-board-layer').getContext('2d');
 
-  context.fillStyle = 'yellow';
-  context.fillRect(50, 200, 100, 100);
-};
+//   context.fillStyle = 'yellow';
+//   context.fillRect(50, 200, 100, 100);
+// };
 
-drawGameBoard();
+// drawGameBoard();
 
-var drawUI = function () {
-  var context = document.getElementById('ui-layer').getContext('2d');
+// var drawUI = function () {
+//   var context = document.getElementById('ui-layer').getContext('2d');
 
-  context.fillStyle = 'red';
-  context.fillRect(200, 200, 100, 100);
-};
+//   context.fillStyle = 'red';
+//   context.fillRect(200, 200, 100, 100);
+// };
 
-drawUI();
+// drawUI();
 
 
 
