ff5092dad9cca0a696359b549b6ede27fef626a0
FabianB14 <32110808+FabianB14@users.noreply.github.com>
2019-06-26 13:22:25 -0700

    working event listeners

--- js/view.js
+++ js/view.js
@@ -3,43 +3,43 @@
 // const canvas = document.getElementById('gameBoard');
 // const ctx = canvas.getContext('2d');
 
-var drawBackground = function () {
-  var context = document.getElementById('background-layer').getContext('2d');
+// var drawBackground = function () {
+//   var context = document.getElementById('background-layer').getContext('2d');
 
-  var gradient = context.createRadialGradient(110, 90, 30, 100, 100, 70);
-  gradient.addColorStop(0, "white");
-  gradient.addColorStop(0.9, "#201d57");
-  gradient.addColorStop(1, "black");
+//   var gradient = context.createRadialGradient(110, 90, 30, 100, 100, 70);
+//   gradient.addColorStop(0, "white");
+//   gradient.addColorStop(0.9, "#201d57");
+//   gradient.addColorStop(1, "black");
 
-  context.fillStyle = gradient;
-  context.fillRect(10, 10, 400, 400);
-};
+//   context.fillStyle = gradient;
+//   context.fillRect(10, 10, 400, 400);
+// };
 
-drawBackground();
+// drawBackground();
 
-var drawGameBoard = function () {
-  var context = document.getElementById('game-board-layer').getContext('2d');
+// var drawGameBoard = function () {
+//   var context = document.getElementById('game-board-layer').getContext('2d');
 
-  context.fillStyle = 'yellow';
-  context.fillRect(50, 200, 100, 100);
-};
+//   context.fillStyle = 'yellow';
+//   context.fillRect(50, 200, 100, 100);
+// };
 
-drawGameBoard();
+// drawGameBoard();
 
-var drawUI = function () {
-  var context = document.getElementById('ui-layer').getContext('2d');
+// var drawUI = function () {
+//   var context = document.getElementById('ui-layer').getContext('2d');
 
-  context.fillStyle = 'red';
-  context.fillRect(200, 200, 100, 100);
-};
+//   context.fillStyle = 'red';
+//   context.fillRect(200, 200, 100, 100);
+// };
 
-drawUI();
+// drawUI();
 
-var firstCardTag = document.getElementById('first-card');
-var secondCardTag = document.getElementById('second-card');
-var thirdCardTag = document.getElementById('third-card');
-var fourthCardTag = document.getElementById('fourth-card');
-var fifthCardTag = document.getElementById('fifth-card');
+// var firstCardTag = document.getElementById('first-card');
+// var secondCardTag = document.getElementById('second-card');
+// var thirdCardTag = document.getElementById('third-card');
+// var fourthCardTag = document.getElementById('fourth-card');
+// var fifthCardTag = document.getElementById('fifth-card');
 
 
 // Make functions to draw these shapes? Hmmmm...IDK we shall see. It's so specific and aren't they all just one time uses? Blah.
