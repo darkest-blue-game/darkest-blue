67d03a29609f19c167a01dd5e74afa4cc00a1f2b
Padmapriya Ganapathi <gpadmapriya@hotmail.com>
2019-06-26 17:29:23 -0700

    Progress bar and draw card methods

--- js/model.js
+++ js/model.js
@@ -13,7 +13,11 @@ var Player = function (name) {
   this.card = Card;
   this.deck = Deck;
   this.discardPile = new Deck;
+<<<<<<< Updated upstream
   this.hand = [];
+=======
+  this.hand = Hand;
+>>>>>>> Stashed changes
   //array of players
   Player.allPlayers.push(this);
 };
@@ -99,21 +103,41 @@ function playCard(currentPlayer, otherPlayer, card) {
   var healthPoints = 0;
   if (currentPlayer.name === 'boss') {
     var index = Math.floor(Math.random() * 5);
+<<<<<<< Updated upstream
     card = currentPlayer.hand.splice(index,1)[0];
     // card = cardArr[0];
     // currentPlayer.hand.allCards.splice(index, 1);
   }
   console.log(card);
+=======
+    card = currentPlayer.hand.splice(index, 1)[0];
+    //card = cardArr[0];
+  }
+  console.log('Big boss card ' + card);
+  var playerHealth = document.getElementById('playerHealth');
+  var opponentHealth = document.getElementById('opponentHealth');
+
+>>>>>>> Stashed changes
   //var newCard = card;
   if (card.cardType === 'positive') {
     console.log(card.cardType);
     healthPoints = currentPlayer.remainingHealthPoints + card.cardWeight;
     currentPlayer.remainingHealthPoints = healthPoints;
+    if (currentPlayer.name === 'Boss') {
+      updateHealth(opponentHealth, card.cardType, card.cardWeight);
+    } else {
+      updateHealth(playerHealth, card.cardType, card.cardWeight);
+    }
   }
   if (card.cardType === 'negative') {
     console.log(card.cardType);
     healthPoints = otherPlayer.remainingHealthPoints - card.cardWeight;
     otherPlayer.remainingHealthPoints = healthPoints;
+    if (currentPlayer.name === 'Boss') {
+      updateHealth(playerHealth, card.cardType, card.cardWeight);
+    } else {
+      updateHealth(opponentHealth, card.cardType, card.cardWeight);
+    }
   }
   currentPlayer.nextTurn = false;
   otherPlayer.nextTurn = true;
@@ -125,8 +149,40 @@ function playCard(currentPlayer, otherPlayer, card) {
 
 function createCards() {
   for (var i = 0; i < classMembers.length; i++) {
-    new Card(classMembers[i], 'positive', Math.floor(Math.random() * 8));
-    new Card(classMembers[i], 'negative', Math.floor(Math.random() * 8));
+    new Card(classMembers[i], 'positive', Math.floor(Math.random() * 7) + 1);
+    new Card(classMembers[i], 'negative', Math.floor(Math.random() * 7) + 1);
   }
 }
 createCards();
+
+function updateHealth(healthElement, cardType, cardWeight) {
+
+  if (cardType === 'positive') {
+    healthElement.value += cardWeight;
+  }
+  if (cardType === 'negative') {
+    healthElement.value -= cardWeight;
+  }
+
+  /*var width = 1;
+  var id = setInterval(frame, 10);
+  function frame() {
+    if (width >= 100) {
+      clearInterval(id);
+    } else {
+      width++;
+      healthElement.style.height = width + '%';
+    }
+  }*/
+}
+
+
+var playerHealth = document.getElementById('opponentHealth');
+updateHealth(playerHealth, 'negative', 3);
+
+var newPlayer = new Player('Padma');
+var Boss = new Player('Boss');
+playCard(Boss, newPlayer, Card.allCards[0]);
+
+
+
