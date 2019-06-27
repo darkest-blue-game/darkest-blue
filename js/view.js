c4f5088bda975710fceaa9a55c958e6aa360f187
Brandon Hurrington <brandonhurrington@yahoo.com>
2019-06-26 17:04:13 -0700

    Major UI overhaul.

--- js/view.js
+++ js/view.js
@@ -3,11 +3,6 @@
 
 
 
-// const canvas = document.getElementById('gameBoard');
-// const ctx = canvas.getContext('2d');
-
-// var drawBackground = function () {
-//   var context = document.getElementById('background-layer').getContext('2d');
 
 //   var gradient = context.createRadialGradient(110, 90, 30, 100, 100, 70);
 //   gradient.addColorStop(0, "white");
@@ -18,25 +13,7 @@
 //   context.fillRect(10, 10, 400, 400);
 // };
 
-// drawBackground();
 
-// var drawGameBoard = function () {
-//   var context = document.getElementById('game-board-layer').getContext('2d');
-
-//   context.fillStyle = 'yellow';
-//   context.fillRect(50, 200, 100, 100);
-// };
-
-// drawGameBoard();
-
-// var drawUI = function () {
-//   var context = document.getElementById('ui-layer').getContext('2d');
-
-//   context.fillStyle = 'red';
-//   context.fillRect(200, 200, 100, 100);
-// };
-
-// drawUI();
 
 var firstCardTag = document.getElementById('first-card');
 var secondCardTag = document.getElementById('second-card');
@@ -44,5 +21,3 @@ var thirdCardTag = document.getElementById('third-card');
 var fourthCardTag = document.getElementById('fourth-card');
 var fifthCardTag = document.getElementById('fifth-card');
 
-
-// Make functions to draw these shapes? Hmmmm...IDK we shall see. It's so specific and aren't they all just one time uses? Blah.
